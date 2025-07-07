5941bb793bd5f13ec4f197700bdd5b967ba55cc1
unknown <sally-draycott@hotmail.co.uk>
2025-07-07 15:43:12 +0100

    challenge 1 completed

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,32 @@
+package com.sdraycott.scalachallenges
+
+/*
+CURRYING - a function that does not take all of its arguments upfront
+This part of the signature  Int => Seq[Int] means that the function returns a function that takes an Int and returns a Seq[Int]
+(multiplier: Int) => ... is an anonymous function that takes multiplier as input.
+arr.map(x => x * multiplier) produces a new sequence by multiplying each element x by multiplier.
+ */
+
+object Main extends App {
+  /*
+  CHALLENGE 1
+  CURRYING - a function that does not take all of its arguments upfront
+  This part of the signature  Int => Seq[Int] means that the function returns a function that takes an Int and returns a Seq[Int]
+  (multiplier: Int) => ... is an anonymous function that takes multiplier as input.
+  arr.map(x => x * multiplier) produces a new sequence by multiplying each element x by multiplier.
+   */
+  def multiplyAll(intArr: Seq[Int]): Int => Seq[Int] = {
+    (multiplier: Int) => intArr.map(x => x * multiplier)
+  }
+
+  val multiplyBy = multiplyAll(Seq(1,2,3))
+  val result = multiplyBy(2)
+  print(result)
+
+  /*
+  CHALLENGE 2
+
+   */
+
+  
+}
