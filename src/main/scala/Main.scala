66ab7d0112bc40e4c7f6352e718f28cca7274178
unknown <sally-draycott@hotmail.co.uk>
2025-07-07 18:47:57 +0100

    completed challenge 5

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,7 +8,7 @@ arr.map(x => x * multiplier) produces a new sequence by multiplying each element
  */
 
 object Main extends App {
-/*
+  /*
 CHALLENGE 1 - Currying functions: multiply all elements in an array
     CURRYING - a function that does not take all of its arguments upfront
     This part of the signature  Int => Seq[Int] means that the function returns a function that takes an Int and returns a Seq[Int]
@@ -19,20 +19,21 @@ CHALLENGE 1 - Currying functions: multiply all elements in an array
     (multiplier: Int) => intArr.map(x => x * multiplier)
   }
 
-  val multiplyBy = multiplyAll(Seq(1,2,3))
+  val multiplyBy = multiplyAll(Seq(1, 2, 3))
   val result = multiplyBy(2)
   println(result)
 
-/*
+  /*
 CHALLENGE 2 - Odd or Even?
 */
 
-  def oddOrEven(xs: Seq[Int]) : String = {
+  def oddOrEven(xs: Seq[Int]): String = {
     if xs.sum % 2 == 0 then "even" else "odd"
   }
+
   println(oddOrEven(Seq()))
 
-/*
+  /*
 CHALLENGE 3 - Find the odd Int
   .groupBy(identity) groups elements based on their value - the result is Map[Int, Seq[Int]] where each key is a value from the list
   and each value is a list of the occurrences of that value e.g.:
@@ -47,17 +48,37 @@ CHALLENGE 3 - Find the odd Int
 */
 
   def findOdd(xs: Seq[Int]): Int = {
-    xs.groupBy(identity).map {case (k, v) => (k, v.size)}.find(odd => odd._2 % 2 !=0).get._1
+    xs.groupBy(identity).map { case (k, v) => (k, v.size) }.find(odd => odd._2 % 2 != 0).get._1
   }
-  val sequence = Seq(1,1,1,1,2,2,3,3,4,5,5,5,5)
+
+  val sequence = Seq(1, 1, 1, 1, 2, 2, 3, 3, 4, 5, 5, 5, 5)
   println(findOdd(sequence))
 
-/*
+  /*
 CHALLENGE 4 - Descending order
+  Didn't need to split it - could have just gone straight for sorted
  */
 
   def descendingOrder(num: Int): Int = {
     num.toString.split("").sorted.reverse.mkString("").toInt
   }
+
   println(descendingOrder(46523))
+
+
+/*
+Challenge 5 - Your order, please
+ */
+  def order(str: String): String = {
+    if (str.length == 0) "" else {
+      str.split(" ").sortBy(word => word.find(_.isDigit).get.asDigit).mkString(" ")
+    }
+  }
+
+  println(order("4of Fo1r pe6ople g3ood th5e the2"))
+
 }
+
+
+
+
